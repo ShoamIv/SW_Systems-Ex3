253857ed0a127970badbcc6fe25ab3901dc19a30
ShoamIv <155763643+ShoamIv@users.noreply.github.com>
2024-02-20 17:18:52 +0200

    Add files via upload

--- Ex3/Main.c
+++ Ex3/Main.c
@@ -8,22 +8,29 @@ scanf("%d",&user);
 StrList* myList= StrList_alloc();
 
 while (user!=0){
-  int word_size=10;
     int len_words;
 if(user==1){
+    int word_size=100;
    char *word= (char*)(malloc)(word_size*sizeof(char));
     scanf("%d",&len_words);
     char ch;
-    for(int i=0; i<len_words; i++){
-      scanf("%c",&ch);
+    int j=0;
+    int counter=0;
+    while(counter!=len_words){
+      scanf(" %c", &ch);
      while(ch!='\n' && ch!=EOF){
+      while(j>=word_size){
+        word_size*=2; 
+        word=(char*)realloc(word,word_size*sizeof(char));
+      }
           if(ch==' '){break;}
-          word[word_size++]=ch;
-          word=(char*)realloc(word,word_size*sizeof(char));
+          word[j++]=ch;
           }
-        word[word_size]='\0';
+        word[j]='\0';
         StrList_insertLast(myList,word);
+        counter++;
           }
+          break;
         }
       
      if(user==2){
@@ -38,41 +45,51 @@ if(user==1){
         int size=StrList_size(myList);
           printf("%d",size);
      }
+
      if(user==4){
       int len=StrList_printLen(myList);
-      printf("%d",len);
+      printf(" %d",len);
      }
+
      if(user==5){
       int index; scanf("%d",&index);
       StrList_printAt(myList,index);
      }
+
      if(user==6){
        StrList_printLen(myList);
      }
+
      if(user==7){
       char* word = (char*)malloc(sizeof(char) * 50);
        scanf("%s\n",word);
       int repeat=StrList_count(myList,word);
       printf("%d",repeat);
      }
+
      if(user==8){
       char* word = (char*)malloc(sizeof(char) * 50);
       scanf("%s\n",word);
       StrList_remove(myList,word);
      }
+
      if(user==9){
        int index; scanf("%d",&index);
        StrList_removeAt(myList,index);
      }
+
      if(user==10){
       StrList_reverse(myList);
      }
+
      if(user==11){
       StrList_free(myList);
      }
+
      if(user==12){
       StrList_sort(myList);
      }
+
      if(user==13){
       StrList_isSorted(myList);
      }
